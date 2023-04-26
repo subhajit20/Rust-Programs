3acca5009dd4d39d53223e99e2af5a800b9b4f8e
subhajit20 <subhajitstd07@gmail.com>
2023-04-27 00:50:07 +0530

    Learnt about Lifetime concept of rust

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,43 @@
-pub mod vector{
-    pub mod vector;
-    pub mod traits;
-}
-// use vector::vector::{get_vec,RUST_VEC};
-use vector::traits::Student;
-
+// pub mod vector{
+//     pub mod vector;
+//     pub mod traits;
+// }
+// // use vector::vector::{get_vec,RUST_VEC};
+// use vector::traits::Student;
 
 fn main(){
-    let std_name = String::from("Subhajit");
-    let std = Student::create_student(&std_name);
 
-    println!("{}",std.firstname);
+    // Lifetime concept
+
+    // This example gives an error 
+    // let y:&u32;           // + ------ 'b - | 
+    // {                     // + -----  'b   |
+    //     let x:u32 = 20;  //                |
+    //     y = &x;          // - ------  'b   |
+    // }                   //                 |
+    //                     //                 |
+    // println!("{y}");    // - -------- 'b - |
+
+    let mut name = String::from("I am");
+    // let n1 = student1(&name);
+    let n2 = student2(&mut name);
+
+    // println!("{n1}");
+    println!("{n2}");
+}
+
+// Define life with 'a or 'any character
+// Using life time means   
+// explicitly declare the life time of variable
+
+// a reference with an explicit lifetime
+fn student1<'a>(x:&'a String) -> &'a String{
+    return x;
+}
+
+// a mutable reference with an explicit lifetime
+fn student2<'a>(x:&'a mut String) -> &'a String{
+    x.push_str(" Subhajit");
+
+    return x;
 }
