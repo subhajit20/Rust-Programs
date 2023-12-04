b07abcb78612aea4f1e6550bae7623f2e217b0af
Subhajit Ghosh <subhajitstd07@gmail.com>
2023-12-04 19:40:07 +0530

    Created a single threaded web server

--- src/hashmap/package/HashMap.rs
+++
@@ -1,7 +0,0 @@
-pub mod StudentHashMap{
-    #[derive(Debug)]
-    pub struct Student{
-        pub name:Option<String>,
-        pub roll:Option<u32>
-    }
-}
--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,41 @@
-pub mod vector{
-    pub mod vector;
-    pub mod traits;
+use std::{
+    fs,
+    io::{BufRead, BufReader, Read, Write},
+    net::{TcpListener, TcpStream},
+};
+
+fn main() {
+    let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
+
+    for stream in listener.incoming() {
+        let stream = stream.unwrap();
+
+        // println!("Connection established! {:#?}", stream);
+        handle_connection(stream);
+    }
 }
-// use vector::vector::{get_vec,RUST_VEC};
-use vector::traits::Student;
 
+fn handle_connection(mut stream: TcpStream) {
+    let buf_reader = BufReader::new(&mut stream);
+    let http_request: Vec<_> = buf_reader
+        .lines()
+        .map(|result| result.unwrap())
+        .take_while(|line| !line.is_empty())
+        .collect();
 
-fn main(){
-    let std_name = String::from("Subhajit");
-    let std = Student::create_student(&std_name);
+    println!("Request: {:#?}", http_request[0]);
+    let mut response: String = "".to_owned();
 
-    println!("{}",std.firstname);
+    if http_request[0] == "GET / HTTP/1.1" {
+        let status_line = "HTTP/1.1 200 OK";
+        let file = fs::read_to_string("index.html").unwrap();
+        let length = file.len();
+        response = format!("{status_line}\r\nContent-Length: {length}\r\n\r\n{file}");
+    } else {
+        let status_line = "HTTP/1.1 404 Not Found";
+        let file = fs::read_to_string("404.html").unwrap();
+        let length = file.len();
+        response = format!("{status_line}\r\nContent-Length: {length}\r\n\r\n{file}");
+    }
+    stream.write_all(response.as_bytes()).unwrap();
 }
